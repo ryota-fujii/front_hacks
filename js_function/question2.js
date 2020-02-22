5b7db163994558f24712217db5cc6d3fa9708c47
Ryota Fujii <rs7328ry@gmail.com>
2020-02-22 17:08:27 +0900

    js関数編完了

--- js_function/question2.js
+++ js_function/question2.js
@@ -1,19 +1,25 @@
 const kaidai_1= (age, callback) => {
-    if(typeof age ==="number"){
+    callback(age)
+}
+const message = (age) =>{
+    if((typeof age) !== "number"){
         alert("数値が入力されていません")
     }
-    else if(age<=20){
+    else if(age>=20){
         alert("値は十以上です")
     }
-    else if(age>=10){
+    else if(age<10){
         alert("値は十未満です")
     }
     else{
         alert("値は20~10です")
     }
 }
-const message = () =>{
-    
-}
 
-kaidai_1(18,message)
+// kaidai_1(1,message)
+
+
+
+(function(x,y){
+    console.log(x,y,x+y);
+})(10,20);
